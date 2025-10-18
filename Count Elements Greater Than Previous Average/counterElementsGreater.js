1bd47b7bea378bccaa68f1bff95674c66d360b76
Carlos-J-Guevara <c.guevara.micciollo@gmail.com>
2025-10-18 22:26:57 +0000

    Working all properly

--- Count Elements Greater Than Previous Average/counterElementsGreater.js
+++ Count Elements Greater Than Previous Average/counterElementsGreater.js
@@ -1,17 +1,31 @@
+'use strict';
 /*
  * Complete the 'countResponseTimeRegressions' function below.
  *
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY responseTimes as parameter.
  */
+
+// Test cases 
+
+// Expected output: 2
 let responseTimes = [100, 200, 150,300];
+
+// Expected output: 3
+const arrayOne = [1, 5, 2, 8, 3, 10];
+
+// Expected output: 2
+const arrayTwo = [10, 5, 1, 0];
+
+// Expected output: 2
+const arrayThree = [1, 2, 3, 4, 5];
 
 function countResponseTimeRegressions(responseTimes) {
     let responseTimesVarInternal = responseTimes;
     let averageArray = 0;
     let numbersGreater = 0;
     
-    // average of the array
+    // average of the array.
     for (const index in responseTimesVarInternal) {
         averageArray += responseTimesVarInternal[index];
     }
@@ -19,7 +33,7 @@ function countResponseTimeRegressions(responseTimes) {
 
     // calculus of the numbers greater than the average. 
     for (const index in responseTimesVarInternal) {
-        if (index > averageArray){
+        if (responseTimesVarInternal[index] > averageArray){
             numbersGreater = numbersGreater + 1;
         }
     }
@@ -27,5 +41,7 @@ function countResponseTimeRegressions(responseTimes) {
 }
 
 console.log(countResponseTimeRegressions(responseTimes))
-
+console.log(countResponseTimeRegressions(arrayOne))
+console.log(countResponseTimeRegressions(arrayTwo))
+console.log(countResponseTimeRegressions(arrayThree))
 
