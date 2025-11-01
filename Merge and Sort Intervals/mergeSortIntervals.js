36025257877b19edce37571f32771cc654a1f3e5
Carlos-J-Guevara <c.guevara.micciollo@gmail.com>
2025-11-01 17:39:26 +0000

    Explanation, and first steps.

---
+++ Merge and Sort Intervals/mergeSortIntervals.js
@@ -0,0 +1,8 @@
+// Merge and Sort Intervals
+// Given an array of intervals [startTime, endTime], merge all overlapping intervals and return a sorted array of non-overlapping intervals.
+
+// Example
+// intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+
+// Output
+// [[1, 6], [8, 10], [15, 18]]
