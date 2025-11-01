99f8c02bcc2085c293643db95f60d66b157ae1c2
Carlos-J-Guevara <c.guevara.micciollo@gmail.com>
2025-11-01 20:00:12 +0000

    Starting and cleaning, defining and structuring.

--- Merge and Sort Intervals/mergeSortIntervals.js
+++ Merge and Sort Intervals/mergeSortIntervals.js
@@ -6,3 +6,37 @@
 
 // Output
 // [[1, 6], [8, 10], [15, 18]]
+
+// I assume it was already sorted somehow
+
+// const intervals = [
+// [1, 3], rows
+// [2, 6], rows
+// [8, 10],rows
+// [15, 18]rows
+
+// cl1 cl2
+// ];
+
+
+const intervals = [
+[1, 3], 
+[2, 6], 
+[8, 10],
+[15, 18]
+];
+
+function mergeHighDefinitionIntervals(intervals) {
+    let intervalsReturn = intervals; 
+    let row = [];
+    let rowFirstElement = 0;
+    let rowSecondElement = 0;
+    let comparing = [];  
+
+    for (const index in intervalsReturn){
+        
+        intervalsReturn[index][index]
+    }
+
+    return
+}
